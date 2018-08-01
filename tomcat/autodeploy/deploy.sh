abeddc0b6d8da861389c88b75fd217146b51b0e7
varun mohan <varunmadhavam@gmail.com>
2018-08-01 14:05:42 +0530

    Update deploy.sh

--- tomcat/autodeploy/deploy.sh
+++ tomcat/autodeploy/deploy.sh
@@ -15,8 +15,8 @@ TOMCAT_PORT=8080
 MANAGER_URL=http://localhost:$TOMCAT_PORT/manager/html
 DEPLOY_WAIT=300
 TOMCAT_LOG=$TOMCAT_VERSION/logs/catalina.out
-#LOG=$APP_ROOT/deploy.log
-LOG=/dev/stdout
+LOG=$APP_ROOT/deploy.log
+#LOG=/dev/stdout
 
 echo >>$LOG
 echo `date +"%b %d %T"`" "`hostname`" $APPID: Starting deployment">>$LOG
@@ -41,15 +41,16 @@ while [ $(ps -ef | grep -i catalina.startup.Bootstrap| grep -v grep|wc -l) -ne 0
     echo `date +"%b %d %T"`" "`hostname`" $APPID: Tomcat didnt stop after 60 seconds...Exiting">>$LOG
     echo `date +"%b %d %T"`" "`hostname`" $APPID: Deployment Failed">>$LOG
     exit
-  else
+  else 
     echo `date +"%b %d %T"`" "`hostname`" $APPID: Waiting 5 seconds for Tomcat Shutdown">>$LOG
     sleep 5
     i=`expr $i + 1`
-  fi
+  fi 
  done
+echo `date +"%b %d %T"`" "`hostname`" $APPID: Tomcat services stopped.">>$LOG
 }
 
-function is_running
+function is_running 
 {
 if [ -f $APP_ROOT/$APPID.pid ]; then
    echo `date +"%b %d %T"`" "`hostname`" $APPID: File $APP_ROOT/$APPID.pid exists">>$LOG
@@ -74,7 +75,7 @@ function do_deploy
 {
 if [ -f $DEPLOY_DIRECTORY/dodeploy ]; then
    echo `date +"%b %d %T"`" "`hostname`" $APPID: Dodeploy flag set..removing it and continuing deployment">>$LOG
-   echo -n `date +"%b %d %T"`" "`hostname`" $APPID: ";rm -v --interactive=never $DEPLOY_DIRECTORY/dodeploy 2>&1>>$LOG
+   echo -n `date +"%b %d %T"`" "`hostname`" $APPID: ">>$LOG;rm -v --interactive=never $DEPLOY_DIRECTORY/dodeploy 2>&1>>$LOG
    if [ $? -ne 0 ];then
     echo `date +"%b %d %T"`" "`hostname`" $APPID: Error removing deploy flag..exiting">>$LOG
     echo `date +"%b %d %T"`" "`hostname`" $APPID: Deployment Failed">>$LOG
@@ -89,18 +90,18 @@ fi
 
 function sync_files
 {
-for x in $(find $DEPLOY_DIRECTORY -type f -name "*.war");
- do
+for x in $(find $DEPLOY_DIRECTORY -type f -name "*.war"); 
+ do  
   file=`basename $x`
-  folder=${file%.*}
+  folder=${file%.*} 
   if [ -f $TARGET_DIRECTORY/$file ]; then
      echo `date +"%b %d %T"`" "`hostname`" $APPID: Backing up file " $TARGET_DIRECTORY/$file " as " $BACKUP/$file.bak>>$LOG
      if [ -f $BACKUP/$file.bak ]; then
       echo `date +"%b %d %T"`" "`hostname`" $APPID: Archiving backup file " $BACKUP/$file.bak " as " $BACKUP/$file.$(date +"%d%b%T")
-      echo -n `date +"%b %d %T"`" "`hostname`" $APPID: ";cp -pv $BACKUP/$file.bak $BACKUP/$file.$(date +"%d%b%T")>>$LOG
-      echo -n `date +"%b %d %T"`" "`hostname`" $APPID: ";cp -pv $TARGET_DIRECTORY/$file $BACKUP/$file.bak>>$LOG
+      echo -n `date +"%b %d %T"`" "`hostname`" $APPID: ">>$LOG;cp -pv $BACKUP/$file.bak $BACKUP/$file.$(date +"%d%b%T")>>$LOG
+      echo -n `date +"%b %d %T"`" "`hostname`" $APPID: ">>$LOG;cp -pv $TARGET_DIRECTORY/$file $BACKUP/$file.bak>>$LOG
      else
-      echo -n `date +"%b %d %T"`" "`hostname`" $APPID: ";cp -pv $TARGET_DIRECTORY/$file $BACKUP/$file.bak>>$LOG
+      echo -n `date +"%b %d %T"`" "`hostname`" $APPID: ">>$LOG;cp -pv $TARGET_DIRECTORY/$file $BACKUP/$file.bak>>$LOG
      fi
      if [ -f $BACKUP/$file.bak ]; then
       echo `date +"%b %d %T"`" "`hostname`" $APPID: File " $file " successfully backed up">>$LOG
@@ -108,11 +109,11 @@ for x in $(find $DEPLOY_DIRECTORY -type f -name "*.war");
       echo `date +"%b %d %T"`" "`hostname`" $APPID: Removing file "$file;rm -rf --interactive=never $TARGET_DIRECTORY/$file 2>&1>>$LOG
       echo `date +"%b %d %T"`" "`hostname`" $APPID: Removing folder "$folder;rm -rf --interactive=never $TARGET_DIRECTORY/$folder 2>&1>>$LOG
       echo `date +"%b %d %T"`" "`hostname`" $APPID: Copying new files for application "$file>>$LOG
-      echo -n `date +"%b %d %T"`" "`hostname`" $APPID: ";cp -pv $DEPLOY_DIRECTORY/$file $TARGET_DIRECTORY>>$LOG
+      echo -n `date +"%b %d %T"`" "`hostname`" $APPID: ">>$LOG;cp -pv $DEPLOY_DIRECTORY/$file $TARGET_DIRECTORY>>$LOG
       if [ -f $TARGET_DIRECTORY/$file ];then
-        echo `date +"%b %d %T"`" "`hostname`" $APPID: Application "$file" successfully copied">>$LOG
+	echo `date +"%b %d %T"`" "`hostname`" $APPID: Application "$file" successfully copied">>$LOG
       else
-        echo `date +"%b %d %T"`" "`hostname`" $APPID: failed to copy application "$file" ....Exiting">>$LOG
+	echo `date +"%b %d %T"`" "`hostname`" $APPID: failed to copy application "$file" ...Exiting">>$LOG
         echo `date +"%b %d %T"`" "`hostname`" $APPID: Deployment Failed">>$LOG
         exit
       fi
@@ -133,7 +134,7 @@ echo `date +"%b %d %T"`" "`hostname`" $APPID: Checking appp deployment status">>
 apps=$(echo $APPS | tr ":" "\n")
 j=0
 retry=true
-while $retry
+while $retry 
 do
 retry=false
   if [ $j -eq $DEPLOY_TIMEOUT ]; then
@@ -144,8 +145,8 @@ retry=false
       j=`expr $j + 1`
   fi
   manager=$(curl --connect-timeout 10 -m 60 -u $MNGUSER:$MNGPASSWD $MANAGER_URL 2>/dev/null)
-  for app  in $apps;
-    do
+  for app  in $apps; 
+    do 
       if [ $(echo "$manager"| grep -w "/$app</a>"|wc -l) -eq 1 ]; then
          echo `date +"%b %d %T"`" "`hostname`" $APPID: Application "$app" deployed">>$LOG
       else
@@ -193,23 +194,25 @@ echo `date +"%b %d %T"`" "`hostname`" $APPID: Port $TOMCAT_PORT is now active">>
 
 function wait_for_deployment
 {
+echo 0 > flag
 echo `date +"%b %d %T"`" "`hostname`" $APPID: Waiting max "$DEPLOY_WAIT" seconds for Tomcat to deploy apps and start">>$LOG
 timeout  $DEPLOY_WAIT tail -n0 -F $TOMCAT_LOG | while read LOGLINE
 do
-   [[ "${LOGLINE}" == *"INFO: Server startup in"* ]]  && pkill -P $$ timeout
+   [[ "${LOGLINE}" == *"INFO: Server startup in"* ]]  && echo 1 > flag && pkill -P $$ timeout
 done
-x=$?
-if [ $x -eq 1 ]; then
+x=$(cat flag)
+if [ $x -eq 0 ]; then
    echo `date +"%b %d %T"`" "`hostname`" $APPID: Tomcat deployment didn't complete after "$DEPLOY_WAIT" seconds...exiting">>$LOG
    echo `date +"%b %d %T"`" "`hostname`" $APPID: Deployment failed">>$LOG
    exit
-elif [ $x -eq 0 ]; then
-   echo `date +"%b %d %T"`" "`hostname`" $APPID: Tomcat service started">>$LOG
+elif [ $x -eq 1 ]; then
+   echo `date +"%b %d %T"`" "`hostname`" $APPID: Tomcat service started">>$LOG 
 else
    echo `date +"%b %d %T"`" "`hostname`" $APPID: Unexpected retunr from timeout...Exiting">>$LOG
    echo `date +"%b %d %T"`" "`hostname`" $APPID: Deployment failed">>$LOG
    exit
 fi
+echo 0 > flag
 }
 
 is_running
